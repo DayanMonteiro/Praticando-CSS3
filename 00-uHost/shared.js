e136ea84ef79f374f97cca9f72d8dc53a8314ac1
Dayan Monteiro <dmsmamb@gmail.com>
2021-05-06 17:29:26 -0300

    expanding javascript

--- 00-uHost/shared.js
+++ 00-uHost/shared.js
@@ -1,6 +1,11 @@
 var backdrop = document.querySelector('.backdrop');
 var modal = document.querySelector('.modal');
+var modalNoButton = document.querySelector('.modal__action--negative');
 var selectPlanButtons = document.querySelectorAll('.plan button');
+var toggleButton = document.querySelector('.toggle-button');
+var mobileNav = document.querySelector('.mobile-nav');
+
+
 
 for ( var i = 0; i < selectPlanButtons.length; i++ ) {
     selectPlanButtons[i].addEventListener('click', function() {
@@ -10,14 +15,22 @@ for ( var i = 0; i < selectPlanButtons.length; i++ ) {
     });
 }
 
-backdrop.addEventListener('click', closeModal);
+backdrop.addEventListener('click', function() {
+    mobileNav.style.display = 'none';
+    this.closeModal();
+});
 
 modalNoButton.addEventListener('click', closeModal);
 
 function closeModal() {
-    
+    backdrop.style.display = 'none';
+    modal.style.display = 'none';
 }
 
+toggleButton.addEventListener('click', function(){
+    mobileNav.style.display = 'block';
+    backdrop.style.display = 'block';
+});
 
 // console.log(backdrop); 
 //backdrop.style.display = 'block';
