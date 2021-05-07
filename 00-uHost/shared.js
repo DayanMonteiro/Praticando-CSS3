e302f99a6cd2e261db58523ac9cd9f3883f56883
Dayan Monteiro <dmsmamb@gmail.com>
2021-05-07 16:19:54 -0300

    manipulating elements classes

--- 00-uHost/shared.js
+++ 00-uHost/shared.js
@@ -9,27 +9,34 @@ var mobileNav = document.querySelector('.mobile-nav');
 
 for ( var i = 0; i < selectPlanButtons.length; i++ ) {
     selectPlanButtons[i].addEventListener('click', function() {
-        modal.style.display = 'block';
-        backdrop.style.display = 'block';
-        
+       // modal.style.display = 'block';
+       // backdrop.style.display = 'block';
+      // modal.className = 'open'; // isso substituiria a lista de classes completa
+        modal.classList.add('open');
+        backdrop.classList.add('open');
     });
 }
 
 backdrop.addEventListener('click', function() {
-    mobileNav.style.display = 'none';
-    this.closeModal();
+   // mobileNav.style.display = 'none';
+    mobileNav.classList.remove('open');
+    closeModal();
 });
 
 modalNoButton.addEventListener('click', closeModal);
 
 function closeModal() {
-    backdrop.style.display = 'none';
-    modal.style.display = 'none';
+   // backdrop.style.display = 'none';
+   // modal.style.display = 'none';
+   modal.classList.remove('open');
+   backdrop.classList.remove('open');
 }
 
 toggleButton.addEventListener('click', function(){
-    mobileNav.style.display = 'block';
-    backdrop.style.display = 'block';
+    // mobileNav.style.display = 'block';
+    // backdrop.style.display = 'block';
+    mobileNav.classList.add('open');
+    backdrop.classList.add('open');
 });
 
 // console.log(backdrop); 
